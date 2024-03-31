1abc4177fec8d828237effd0008913e35ee505a6
Arvianto D. Wicaksono <saya@arvian.to>
2024-04-01 03:16:10 +0700

    init: config and utilities

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,7 +1,44 @@
+import path from 'node:path';
+
+/**
+ * @param {string[]} filenames
+ */
+function buildEslintCommand(filenames) {
+  const stagedFiles = filenames.map((file) => path.relative(process.cwd(), file)).join(' --file ');
+  const options = [
+    '--cache-location .cache/.eslintcache',
+    '--cache-strategy metadata',
+    '--cache',
+    '--dir src',
+    '--fix',
+    // keep --file option last
+    '--file',
+  ].join(' ');
+  const command = `next lint ${options} ${stagedFiles}`;
+
+  return command;
+}
+
+/**
+ * @param {string[]} filenames
+ */
+function buildPrettierCommand(filenames) {
+  const stagedFiles = filenames.map((file) => path.relative(process.cwd(), file)).join(' ');
+  const options = [
+    '--cache-location .cache/.prettiercache',
+    '--cache-strategy metadata',
+    '--cache',
+    '--write',
+  ].join(' ');
+  const command = `npx prettier ${options} ${stagedFiles}`;
+
+  return command;
+}
+
 /** @type {import('lint-staged').Config} */
 const lintStagedConfig = {
-    'src/**/*.{ts,tsx}': [],
-    '**/*.{css,cjs,json,md,mjs}': []
+  'src/**/*.{ts,tsx}': [buildEslintCommand, buildPrettierCommand],
+  '**/*.{css,cjs,json,md,mjs}': [buildPrettierCommand],
 };
 
 export default lintStagedConfig;
