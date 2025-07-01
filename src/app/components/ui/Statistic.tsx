fcf690c84503b9afbfbc37dc0e0f28a050099a7f
Arvianto D. Wicaksono <dev@arvian.to>
2025-07-01 14:43:19 +0700

    fix(statistics): apply tabular numeric font variant

--- src/app/components/ui/Statistic.tsx
+++ src/app/components/ui/Statistic.tsx
@@ -12,8 +12,13 @@ interface Props {
 
 export const Statistic: FC<Props> = ({ className, text, value }) => {
   return (
-    <div className={cx("flex grow basis-0 flex-col gap-2 font-general", className)}>
-      <CountUp start={0} end={value} suffix="+" className="text-4xl font-bold border-b-2 border-b-gold w-fit" />
+    <div className={cx('flex flex-col gap-2 font-general', className)}>
+      <CountUp
+        start={0}
+        end={value}
+        suffix="+"
+        className="self-start border-b-2 border-b-gold text-4xl font-bold tabular-nums"
+      />
       <div className="text-[14px] whitespace-nowrap opacity-60">{text}</div>
     </div>
   );
