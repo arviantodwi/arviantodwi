c9d026980fc9a189159251dc67e99a0f4ea614c6
Arvianto D. Wicaksono <dev@arvian.to>
2025-07-05 13:20:51 +0700

    fix: eslint error on build

--- src/app/components/ui/SocialNav.tsx
+++ src/app/components/ui/SocialNav.tsx
@@ -3,11 +3,9 @@ import { FC, HTMLAttributes } from 'react';
 import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 import { GITHUB_URL, LINKEDIN_URL } from '../../libs/constants';
 
-interface Props extends HTMLAttributes<HTMLDivElement> {}
-
-export const SocialNav: FC<Props> = ({ className, ...props }) => {
+export const SocialNav: FC<HTMLAttributes<HTMLDivElement>> = ({ className, ...props }) => {
   return (
-    <div className={cx('flex justify-center gap-3 items-center h-fit', className)} {...props}>
+    <div className={cx('flex h-fit items-center justify-center gap-3', className)} {...props}>
       <a href={GITHUB_URL} target="_blank" rel="noopener noreferrer" title="Arvianto's GitHub">
         <AiFillGithub size={24} />
       </a>
